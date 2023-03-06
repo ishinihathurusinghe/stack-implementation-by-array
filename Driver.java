a1efcce48b6271f1704697332c17e6486e139617
Ishini Hathurusinghe <ishinihathurusinghe@gmail.com>
2023-03-06 14:59:03 +0530

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,13 @@
+public class Driver{
+    public static void main(String args[]){
+        Stack s = new Stack();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+
+        System.out.println(s.pop() + "popped from stack");
+
+        System.out.println("Stack is empty: " + s.isEmpty());
+        System.out.println("Stack size: " + s.size());
+    }
+}
---
+++ Stack.java
@@ -0,0 +1,53 @@
+public class Stack{
+    static final int MAX = 1000;
+    int top;
+    int a[] = new int[MAX];
+
+    boolean isEmpty(){
+
+        return(top < 0);
+    }
+
+    int size() {
+        return top + 1;
+    }
+
+    Stack(){
+        top = -1;
+    }
+
+    boolean push(int x){
+        if(top >= (MAX-1)){
+            System.out.println("stack overflow");
+            return false;
+        }
+        else{
+            a[++top] = x;
+            System.out.println(x+"pushed into stack");
+            return true;
+        }
+    }
+
+    int pop(){
+        if(top < 0){
+            System.out.println("stack underflow");
+            return 0;
+        }
+        else{
+            int x = a[top--];
+            return x;
+        }
+    }
+
+    int peek(){
+        if(top < 0){
+            System.out.println("stack underflow");
+            return 0;
+        }
+        else{
+            int x = a[top];
+            return x;
+        }
+    }
+
+}
