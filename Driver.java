159d58eb20e7693e605aca7d344eac86b32ca1d1
Ishini Hathurusinghe <ishinihathurusinghe@gmail.com>
2023-03-07 06:38:24 +0530

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,4 +1,4 @@
-                                            public class Driver{
+                                             public class Driver{
     public static void main(String args[]){
         Stack s = new Stack();
         s.push(10);
